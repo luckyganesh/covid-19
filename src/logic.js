3d4652736716bcc878448cc79df37a6bf4199ca3
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2020-05-20 08:02:34 +0530

    Add active cases in states and districts, add percentage in total.

--- src/logic.js
+++ src/logic.js
@@ -1,56 +1,67 @@
 const { table } = require('table')
 const configuration = require('../config.json');
-const districtsToMonitor = configuration.districts;
-const statesToMonitor = configuration.states;
 
-const countConfirmed = function (districtsData) {
-  return districtsData.reduce((count, dirstrictData) => {
-    return count + dirstrictData["confirmed"]
-  }, 0)
+const countField = function (districtData, field) {
+    return districtData.reduce((count, districtData) => {
+        return count + districtData[ field ]
+    }, 0)
 }
 
-const provide = function(data,element) {
-  return [element, data[element]]
+const provideMultiple = function (data, fields, element) {
+    const myData = data[ element ];
+    return [ element, ...fields.map(field => myData[ field ]) ]
 }
 
 const showStatesTable = function (data) {
-  const statesData = {}
-  data.forEach(({ state, districtData }) => {
-    statesData[state] = countConfirmed(districtData)
-  })
-  const tableData = configuration.states.map(provide.bind(null,statesData))
-  tableData.unshift(["State", "confirmed"])
-  console.log(table(tableData))
+    const statesData = {}
+    const fields = [ "confirmed", "active" ]
+    data.forEach(({ state, districtData }) => {
+        const stateData = {}
+        fields.forEach(field => {
+            stateData[ field ] = countField(districtData, field);
+        })
+        statesData[ state ] = stateData;
+    })
+    const tableData = configuration.states.map(provideMultiple.bind(null, statesData, fields))
+    tableData.unshift([ "State", ...fields ])
+    console.log(table(tableData))
 }
 
 const showDistrictsTable = function (data) {
-  const districtsData = {}
-  data.forEach(({ districtData }) => {
-    districtData.forEach(({ district, confirmed }) => {
-      districtsData[district] = (districtData[district] || 0) + confirmed
+    const districtsData = {}
+    const fields = [ "confirmed", "active" ]
+    data.forEach(({ districtData }) => {
+        districtData.forEach((district) => {
+            const requiredData = {};
+            fields.forEach(field => {
+                requiredData[ field ] = district[ field ]
+            })
+            districtsData[ district[ "district" ] ] = requiredData
+        })
     })
-  })
-  const tableData = configuration.districts.map(provide.bind(null,districtsData))
-  tableData.unshift(["District", "Confirmed"])
-  console.log(table(tableData))
+    const tableData = configuration.districts.map(provideMultiple.bind(null, districtsData, fields))
+    tableData.unshift([ "District", ...fields ])
+    console.log(table(tableData))
 }
 
-const showTotalTable = function(data){
-  const tableData = []
-  tableData.push(["Total", "Analysis"])
-  const analysisFields = ["confirmed", "active", "recovered" , "deaths"];
-  analysisFields.forEach(x => {
-    tableData.push([x, data[x]])
-  })
-  console.log(table(tableData))
+const showTotalTable = function (data) {
+    const tableData = []
+    tableData.push([ "Total", "Analysis", "Percentage" ])
+    const analysisFields = [ "confirmed", "active", "recovered", "deaths" ];
+    const confirmed = data["confirmed"];
+    analysisFields.forEach(x => {
+        const count = data[ x ];
+        tableData.push([ x,count , (count*100/confirmed).toFixed(2)  ])
+    })
+    console.log(table(tableData))
 }
 
 const processData = function (totalData, districtData) {
-  showTotalTable(totalData["statewise"].find(x => x["state"] == "Total"))
-  showStatesTable(districtData)
-  showDistrictsTable(districtData)
+    showTotalTable(totalData[ "statewise" ].find(x => x[ "state" ] === "Total"))
+    showStatesTable(districtData)
+    showDistrictsTable(districtData)
 }
 
 module.exports = {
-  processData
+    processData
 }
